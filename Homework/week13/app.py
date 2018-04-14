33b20c5616c6ca9064f4261fde43908ef2ee48c8
mpaige87 <marsalis.paige@gmail.com>
2018-04-14 11:26:55 -0700

    correct flask app execution

--- Homework/week13/app.py
+++ Homework/week13/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
@@ -9,8 +9,10 @@ mongo = PyMongo(app)
 @app.route('/')
 def index():
     mars_db = mongo.db.mars_db.find_one()
+    print("MONGODB GET*******************")
+    print(mars_db)
     return render_template('index.html', mars_db=mars_db)
-
+    print("RETURN EXECUTED")
 
 @app.route('/scrape')
 def scrape():
@@ -23,7 +25,7 @@ def scrape():
         data,
         upsert=True
     )
-    return redirect("http://localhost:5000/", code=302)
+    return redirect("http://127.0.0.1:5000/", code=302)
 
 
 if __name__ == "__main__":
